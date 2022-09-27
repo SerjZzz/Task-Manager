696922064d49ad623c187bc86cecdea5de9ec08f
Serj Zzz <s.zavolzhskiy@gmail.com>
2022-09-27 23:24:43 +0400

    fix code-style issues with rubocop

--- test/factories/sequence.rb
+++ test/factories/sequence.rb
@@ -7,7 +7,7 @@ FactoryBot.define do
     "person#{n}@gmail.com"
   end
 
-  sequence :state, %i[new_task in_development in_qa in_code_review ready_for_release released archived].cycle
+  sequence :state, [:new_task, :in_development, :in_qa, :in_code_review, :ready_for_release, :released, :archived].cycle
 
   sequence :expired_at do |n|
     Time.now + (60 * 60 * 24 * n)
