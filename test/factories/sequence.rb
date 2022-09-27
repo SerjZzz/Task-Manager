1024594b8f26e6ad9029ccc8f20e5a4ab857e5e3
Serj Zzz <s.zavolzhskiy@gmail.com>
2022-09-27 23:10:11 +0400

    update default :task factory attributes with sequences

--- test/factories/sequence.rb
+++ test/factories/sequence.rb
@@ -1,9 +1,15 @@
 FactoryBot.define do
-  sequence :string, aliases: [:first_name, :last_name, :password, :avatar] do |n|
+  sequence :string, aliases: [:first_name, :last_name, :password, :avatar, :name, :description] do |n|
     "string#{n}"
   end
 
   sequence :email do |n|
     "person#{n}@gmail.com"
+  end
+
+  sequence :state, %i[new_task in_development in_qa in_code_review ready_for_release released archived].cycle
+
+  sequence :expired_at do |n|
+    Time.now + (60 * 60 * 24 * n)
   end
 end
