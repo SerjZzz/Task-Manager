dfea99eae69cb2ccced0ac0b7e5af0839e3513db
Serj Zzz <s.zavolzhskiy@gmail.com>
2022-11-14 23:48:04 +0400

    add custom styles theme via @meterial-ui/core/styles

--- app/javascript/App.js
+++ app/javascript/App.js
@@ -2,12 +2,16 @@ import React from 'react';
 
 import store from 'store';
 import { Provider } from 'react-redux';
-import TaskBoard from 'components/TaskBoard';
+import TaskBoard from 'containers/TaskBoard';
+import { ThemeProvider } from '@material-ui/core/styles';
+import AppTheme from 'themes/AppTheme';
 
 function App() {
   return (
     <Provider store={store}>
-      <TaskBoard />
+      <ThemeProvider theme={AppTheme}>
+        <TaskBoard />
+      </ThemeProvider>
     </Provider>
   );
 }
