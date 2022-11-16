9a693a464ec17e2215906e2f665ad6e839c67bb3
Serj Zzz <s.zavolzhskiy@gmail.com>
2022-11-16 12:05:04 +0400

    add uffizzi host to application development environment config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -92,4 +92,7 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  # Add Uffizzi.com host to application hosts
+  config.hosts << '.app.uffizzi.com'
 end
