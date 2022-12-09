55bc49daab93b2a31e11858ab7ef4250554b5cfa
Serj Zzz <s.zavolzhskiy@gmail.com>
2022-12-09 22:05:03 +0400

    generate db migration for password reset

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_27_162402) do
+ActiveRecord::Schema.define(version: 2022_12_09_175633) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 2022_09_27_162402) do
     t.string "type"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_reset_token_digest"
   end
 
 end
